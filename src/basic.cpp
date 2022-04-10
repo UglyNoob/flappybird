289bdaf8cfed3e0773a5bf58ee35aa642e3e186d
UglyNoob <1253328221@qq.com>
2022-04-10 16:25:00 +0800

    Windows support for log_*s

--- src/basic.cpp
+++ src/basic.cpp
@@ -3,10 +3,6 @@
 #include <cstdarg>
 #include <cstdio>
 
-#ifdef __WIN32
-
-#else
-
 void log_normal(const char *fmt, ...) {
 	va_list vlist;
 	va_start(vlist, fmt);
@@ -14,6 +10,41 @@ void log_normal(const char *fmt, ...) {
 	fputc('\n', stderr);
 	va_end(vlist);
 }
+
+#ifdef __WIN32
+
+#include <windows.h>
+
+void log_warning(const char *fmt, ...) {
+	va_list args;
+	va_start(args, fmt);
+	HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
+	CONSOLE_SCREEN_BUFFER_INFO info;
+	GetConsoleScreenBufferInfo(handle, &info);
+	WORD general = info.wAttributes;
+	SetConsoleTextAttribute(handle, FOREGROUND_RED | FOREGROUND_GREEN);
+	vfprintf(stderr, fmt, args);
+	SetConsoleTextAttribute(handle, general);
+	fprintf(stderr, "\n");
+	va_end(args);
+}
+
+void log_error(const char *fmt, ...) {
+	va_list args;
+	va_start(args, fmt);
+	HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
+	CONSOLE_SCREEN_BUFFER_INFO info;
+	GetConsoleScreenBufferInfo(handle, &info);
+	WORD general = info.wAttributes;
+	SetConsoleTextAttribute(handle, FOREGROUND_RED);
+	vfprintf(stderr, fmt, args);
+	SetConsoleTextAttribute(handle, general);
+	fprintf(stderr, "\n");
+	va_end(args);
+}
+
+
+#else
 
 void log_warning(const char *fmt, ...) {
 	va_list vlist;
